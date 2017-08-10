d037faa0f6f20c11c1758c903d98f45c181fffcc
Sergio Garcia Prado <sergio@garciparedes.me>
2017-08-10 16:54:52 +0200

    Added members

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,25 @@ import {Link} from "./shared/link";
 export class AppComponent {
   title = 'app';
   memberList: Array<Member> = [
-    new Member("Sergio",
+    new Member("Berto"),
+    new Member("Edu"),
+    new Member("Emi"),
+    new Member("Fernando"),
+    new Member("Gamby"),
+    new Member("Garcy",
       [
         new Link("https://facebook.com/garciparedes", "Facebook"),
         new Link("https://instagram.com/garciparedes", "Instagram"),
       ]),
-    new Member("Mario")];
+    new Member("Hector"),
+    new Member("Jorgito"),
+    new Member("Juan"),
+    new Member("Mario"),
+    new Member("Rivas"),
+    new Member("Rome"),
+    new Member("Santy"),
+    new Member("Soul"),
+    new Member("Yimi"),
+    new Member("Vitu"),
+  ];
 }
