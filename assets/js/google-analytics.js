dacc97646d4a293aac894267b783198168145a77
Sergio Garcia Prado <sergio@garciparedes.me>
2017-08-29 19:06:30 +0200

    Updates

--- assets/js/google-analytics.js
+++ assets/js/google-analytics.js
@@ -9,4 +9,5 @@
   a.src = g;
   m.parentNode.insertBefore(a, m)
 })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-ga('create', 'UA-105604677-1', 'none');
+ga('create', 'UA-105604677-1', 'auto');
+ga('send', 'pageview');
