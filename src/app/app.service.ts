cda8fb893d1882f1f27a5efa1201c68862614764
Sergio Garcia Prado <sergio@garciparedes.me>
2017-08-31 17:27:47 +0200

    Created JSON files

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,8 +1,22 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Http} from "@angular/http";
+import {Observable} from 'rxjs/Rx';
 
 @Injectable()
 export class AppService {
 
-  constructor() { }
+  constructor(private http: Http) {
+  }
 
+  get memberObservable(): Observable<any> {
+    return this.http.get('assets/data/members.json');
+  }
+
+  get siteObservable(): Observable<any> {
+    return this.http.get('assets/data/site.json');
+  }
+
+  get memberList() {
+    return this.memberObservable.map(res => res.json());
+  }
 }
