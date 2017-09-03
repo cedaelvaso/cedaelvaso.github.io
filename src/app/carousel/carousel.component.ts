425c8a3cc0df00601f6026c578f9acfeabc80760
Sergio Garcia Prado <sergio@garciparedes.me>
2017-09-03 14:30:03 +0200

    Working on carousel

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,13 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.scss']
+  styleUrls: ['./carousel.component.scss'],
+  providers: [NgbCarouselConfig],
 })
 export class CarouselComponent implements OnInit {
 
-  constructor() { }
+  constructor(config: NgbCarouselConfig) {
+    config.interval = 3000;
+    config.wrap = true;
+    config.keyboard = false;
+  }
 
   ngOnInit() {
   }
